eda32ec1d2732a1992344d8e6b6e4ca8eb452e19
mrgilb <ilya.nikolenko.job@gmail.com>
2022-04-19 20:04:24 +0300

    add resize event

--- source/js/modules/headerFixed.js
+++ source/js/modules/headerFixed.js
@@ -9,6 +9,12 @@ if (document.querySelector('.background-header')) {
       const scroll = window.scrollY;
       const desktopWidth = 1024;
 
+      window.addEventListener('resize', ()=> {
+        if (window.innerWidth < desktopWidth) {
+          background.style.paddingTop = 0;
+        }
+      });
+
       if (window.innerWidth >= desktopWidth) {
         if (scroll > headerHeight) {
           header.classList.add('page-header--fixed');
